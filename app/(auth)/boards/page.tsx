7578c0ae6c76bfb26366392ef7d9f7f485aa28e6
Yefry Sanchez <yefrymsp@gmail.com>
2025-05-24 20:24:41 -0400

    add padding styles

--- app/(auth)/boards/page.tsx
+++ app/(auth)/boards/page.tsx
@@ -6,24 +6,24 @@ const Page = () => {
   const boards = [
     {
       title: "Software Engineer",
-      url: "/boards",
+      url: "/boards/board",
     },
     {
       title: "Sales Engineer",
-      url: "/boards",
+      url: "/boards/board",
     },
     {
       title: "Network Engineer",
-      url: "/boards",
+      url: "/boards/board",
     },
     {
       title: "Cloud Engineer",
-      url: "/boards",
+      url: "/boards/board",
     },
   ];
 
   return (
-    <div className="">
+    <div className="pt-12 px-4">
       <h1 className="font-bold tracking-tighter text-4xl">Boards Hub</h1>
       <section className="mt-8 grid gap-4 md:grid-cols-2 lg:grid-cols-3 max-w-3xl">
         <CreateBoard />
