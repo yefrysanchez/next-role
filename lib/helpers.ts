4ddfa53dbf99508b4bd513c1af7ff720f909a875
Yefry Sanchez <yefrymsp@gmail.com>
2025-06-25 20:37:58 -0400

    create helpers for icons selection

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,5 +1,24 @@
-import slugify from "slugify"
+import slugify from "slugify";
+import * as LucideIcons from "lucide-react"; // Import all icons as a namespace
 
 export function getSlug(id: string, title: string): string {
-  return `${id}-${slugify(title, { lower: true, strict: true })}`
+  return `${id}-${slugify(title, { lower: true, strict: true })}`;
 }
+
+// Type for the valid icon names
+type LucideIconNames = keyof typeof LucideIcons;
+
+// Function that returns the corresponding Lucide icon component
+export const getLucideIcon = (iconName: LucideIconNames) => {
+  const IconComponent = LucideIcons[iconName];
+
+  if (IconComponent) {
+    return IconComponent;
+  } else {
+    console.error(`Icon "${iconName}" not found`);
+    return null; // Return null if icon is not found
+  }
+};
+
+// Create an array of icon names
+ export const iconNames: LucideIconNames[] = Object.keys(LucideIcons) as LucideIconNames[];
