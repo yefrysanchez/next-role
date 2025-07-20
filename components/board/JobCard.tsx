d6fc1954daf7f7f4170e3a0a31bb425c63503d18
Yefry Sanchez <yefrymsp@gmail.com>
2025-07-20 13:53:59 -0400

    changed drag icon

--- components/board/JobCard.tsx
+++ components/board/JobCard.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { ArrowRightLeft, ExternalLink, Eye } from "lucide-react";
+import {
+  ArrowRightLeft,
+  ExternalLink,
+  Eye,
+  GripHorizontal,
+} from "lucide-react";
 import React from "react";
 
 import {
@@ -35,8 +40,8 @@ const JobCard = ({ job }: JobCardProps) => {
   };
 
   return (
-    <div className="bg-gray-50 group w-full p-4 rounded-md grid gap-1 pb-8 group relative hover:bg-gray-100 cursor-grab active:cursor-grabbing active:opacity-65">
-      <JobDetails job={job}>
+    <JobDetails job={job}>
+      <div  className="bg-gray-50 group w-full p-4 rounded-md grid gap-1 pb-8 group relative hover:bg-gray-100 active:cursor-grabbing active:opacity-65">
         <h3 className="text-sm font-semibold flex items-center gap-2">
           <span>{job?.title}</span>
           <span className=" text-gray-400 transition-opacity duration-200 xl:opacity-0 group-hover:opacity-100">
@@ -47,57 +52,60 @@ const JobCard = ({ job }: JobCardProps) => {
           {job?.company}
         </p>
         <p className="text-green-600 text-xs">
-          {job?.salary ? `$ ${job?.salary}` : ""}
+          {job?.salary ? `$${job?.salary}` : ""}
         </p>
         <Badge variant="outline">
           <span className="scale-85 capitalize">
             {handleModality(job?.modality || "Unknown Location")}
           </span>
         </Badge>
-      </JobDetails>
-      <div className="xl:opacity-0 gap-1 group-hover:opacity-100 flex absolute right-2 bottom-2 transition-opacity duration-200">
-        {job?.url ? (
-          <a
-            href={getFormattedUrl(job.url)}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-100 hover:bg-blue-100 p-1 rounded-sm cursor-pointer"
-          >
-            <ExternalLink size={15} />
-          </a>
-        ) : (
-          <button
-            type="button"
-            onClick={() =>
-              toast.error(
-                "No URL provided for this job. Please add a URL to view the job posting."
-              )
-            }
-            className="bg-gray-100 hover:bg-blue-100 p-1 rounded-sm cursor-pointer"
-          >
-            <ExternalLink size={15} />
-          </button>
-        )}
-        <EditJob job={job} />
 
-        <DeleteJob job={job} />
-      </div>
+        <div onClick={(e) => e.stopPropagation()} className="xl:opacity-0 gap-1 group-hover:opacity-100 flex absolute right-2 bottom-2 transition-opacity duration-200 z-40">
+          {job?.url ? (
+            <a
+              href={getFormattedUrl(job.url)}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-100 hover:bg-blue-100 p-1 rounded-sm cursor-pointer"
+            >
+              <ExternalLink size={15} />
+            </a>
+          ) : (
+            <button
+              type="button"
+              onClick={() =>
+                toast.error(
+                  "No URL provided for this job. Please add a URL to view the job posting."
+                )
+              }
+              className="bg-gray-100 hover:bg-blue-100 p-1 rounded-sm cursor-pointer"
+            >
+              <ExternalLink size={15} />
+            </button>
+          )}
+          <EditJob job={job} />
 
-      <DropdownMenu>
-        <DropdownMenuTrigger className="absolute right-2 top-2 xl:hidden">
-          <ArrowRightLeft size={15} />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel className="font-bold">Move to</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {columns.map((move) => (
-            <DropdownMenuItem key={move} className="capitalize">
-              {move}
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+          <DeleteJob job={job} />
+        </div>
+
+        <GripHorizontal onClick={(e) => e.stopPropagation()} className="absolute right-2 top-2 hidden xl:block hover:cursor-grab active:cursor-grabbing" />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="absolute right-2 top-2 xl:hidden">
+            <ArrowRightLeft size={15} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel className="font-bold">Move to</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {columns.map((move) => (
+              <DropdownMenuItem key={move} className="capitalize">
+                {move}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>{" "}
+    </JobDetails>
   );
 };
 
